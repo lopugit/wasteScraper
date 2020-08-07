2e79ef3ddf71a08c3edfb62d823b1f8f5eef985e
Alvin Zhao <ayzee@hotmail.com>
2020-08-07 13:56:27 +1000

    Web Scraper
    Grabs names of items off Can I Recycle This website

---
+++ waste.js
@@ -0,0 +1,34 @@
+// With help from https://www.scrapehero.com/how-to-build-a-web-scraper-using-puppeteer-and-node-js/
+const puppeteer = require('puppeteer');
+
+let pageUrl = 'https://www.recycling.vic.gov.au/can-i-recycle-this';
+(async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.goto(pageUrl);
+
+    // get page details
+    let itemData = await page.evaluate(() => {
+        let items = [];
+        // get the page elements
+        let itemElem = document.querySelectorAll('div.tile');
+        console.log(itemElem);
+        // get the page data
+        itemElem.forEach((item) => {
+            let itemJson = {};
+            try {
+                itemJson.name = tile.querySelector('span.tile__title').innerText;
+                //itemJson.alias = tile.querySelector('span.tile__subtitle').innerText;
+                
+            }
+            catch (exception){
+
+            }
+            items.push(itemJson);
+        });
+        return items;
+    });
+
+    console.dir(itemData);
+})();
