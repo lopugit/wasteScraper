c26f0ffa92a22b4c2ebfb1cdf18bd6aa0f945c43
Nikolaj Frey <lopudesigns@gmail.com>
2020-08-10 13:51:04 +1000

    Added dbSync.js file for taking scraped data, wrangling it a little bit to be more graph like, and uploading to cloud mongodb cluster, items are id'd by category and name, and upserted, meaning if an existing entry is found it is updated and if no existing entry is found it is newly inserted

---
+++ scraper/dbSync.js
@@ -0,0 +1,89 @@
+
+console.log("Starting wasteScraper dbSync...")
+
+// so process never dies
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ', err);
+});
+
+console.log("Started wasteScraper dbSync!")
+
+let json = require('../data.json')
+let fs = require('fs')
+let config = require('config')
+// Parse scraped data for better databasing
+
+let parsed = []
+
+Object.keys(json).forEach((categoryName)=>{
+	let category = json[categoryName]
+	Object.keys(category).forEach(itemName=>{
+		let item = {
+			name: itemName,
+			category: categoryName,
+			...category[itemName]
+		}
+
+		if(item.alias){
+			item.aliases = item.alias.split(",")
+			
+			item.aliases.forEach((e,i)=>{
+				item.aliases[i] = e.trim().replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));
+			})
+
+			delete item.alias
+		}
+		
+		parsed.push(item)
+
+	})
+})
+
+fs.writeFileSync(__dirname+'/../dbData.json', JSON.stringify(parsed))
+
+// create mongoDB connection
+let MongoClient = require('mongodb').MongoClient;
+let uri = config.uri
+let client = new MongoClient(uri, { 
+	useNewUrlParser: true, 
+	useUnifiedTopology: true 
+});
+
+// connect to mongodb connection
+client.connect(async err => {
+	if(err) console.error(err)
+	
+  let db = client.db("wastee");
+	let items = db.collection("items")
+
+	await asyncForEach(parsed, async item=>{
+		let found = await items.findOneAndUpdate(
+			{
+				name: item.name,
+				category: item.category
+			}, 
+			{
+				$set: item
+			},
+			{
+				upsert: true,
+				returnNewDocument: true
+			}).catch(console.error)
+		console.log(found)
+	})
+	
+  // close the connection
+  client.close();
+	
+});
+
+
+async function asyncForEach(array, callback) {
+	
+  for (let index = 0; index < array.length; index++) {
+		
+    await callback(array[index], index, array);
+		
+  }
+	
+}
